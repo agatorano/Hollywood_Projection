7269000a61b8705d4c25d58808537ce288e0421e
Andre Gatorano <agatorano@gmail.com>
2015-10-06 17:35:37 -0400

    yeilds item

--- hollywood_crawler/hollywood_crawler/spiders/director_spider.py
+++ hollywood_crawler/hollywood_crawler/spiders/director_spider.py
@@ -23,24 +23,37 @@ class BoxofficeSpider(Spider):
 
         """
         item = HollywoodItem()
-        name = response.xpath('//td/h1/text()').extract_first()
 
-        item['name'] = name
-        dates = response.xpath('//table[1]/tr/td/font/a/text()|//table[1]/tr/td/font/text()').re(r'^\d+/\S*')
-        first_d = datetime.strptime(dates[0], "%m/%d/%y")
-        last_d = datetime.strptime(dates[-1], "%m/%d/%y")
-        years = (first_d-last_d)
-        years = years.days/365
+        item['name'] = get_name(response)
+        item['years_active'] = get_years(response)
+        item['average_gross'] = get_ave_gross(response)
 
-        item['years_active'] = years
-
-        ave_gross = response.xpath('//td[1]/font/b').re(r'Average: (\S*\d)')[0]
-        ave_gross = int(ave_gross.replace("$", "").replace(",", ""))
-
-        item['average_gross'] = ave_gross
-
-        print name, years, ave_gross
+        yield item
 
 
+def get_name(response):
+
+    name = response.xpath('//td/h1/text()').extract_first()
+
+    return name
+
+
+def get_years(response):
+
+    dates = response.xpath('//table[1]/tr/td/font/a/text()|//table[1]/tr/td/font/text()').re(r'^\d+/\S*')
+    first_d = datetime.strptime(dates[0], "%m/%d/%y")
+    last_d = datetime.strptime(dates[-1], "%m/%d/%y")
+    years = (first_d-last_d)
+    years = years.days/365
+
+    return years
+
+
+def get_ave_gross(response):
+
+    ave_gross = response.xpath('//td[1]/font/b').re(r'Average: (\S*\d)')[0]
+    ave_gross = int(ave_gross.replace("$", "").replace(",", ""))
+
+    return ave_gross
 
         
