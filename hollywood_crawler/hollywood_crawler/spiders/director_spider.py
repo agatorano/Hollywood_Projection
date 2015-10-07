ecf92ce5b4e659ce4d6ac97828e44308a4670ec8
Andre Gatorano <agatorano@gmail.com>
2015-10-06 20:55:22 -0400

    it works :)

--- hollywood_crawler/hollywood_crawler/spiders/director_spider.py
+++ hollywood_crawler/hollywood_crawler/spiders/director_spider.py
@@ -31,24 +31,25 @@ class BoxofficeSpider(Spider):
         links = response.xpath('//td/font/a[contains(@href,"chart")]/@href').extract()
         for href in links:
             url = response.urljoin(href)
-            print ("*********\n\n%s\n\n*********"%url)
+            print ("*********\n\n%s\n\n*********" % url)
             yield scrapy.Request(url, callback=self.parse_director_page)
 
         pages = response.xpath('//font[@size=4]/b/a/@href').extract()
-        
+        next_page = ""
+
         for page in pages:
             page = response.urljoin(page)
             if page not in self.page_seen:
                 next_page = page
                 print ("################\n\n%s\n\n################"%next_page)
                 self.page_seen.add(page)
+                print self.page_seen
                 break
             else:
-                print "last page"
+                next
 
-
-        yield scrapy.Request(next_page, callback=self.parse)
-
+        if len(next_page) > 0:
+            yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_director_page(self, response):
 
